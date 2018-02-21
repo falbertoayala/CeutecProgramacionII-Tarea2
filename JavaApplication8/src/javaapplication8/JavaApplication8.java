c8132172916943f4a016a6be1ce7909be2a7c450
Francisco <Francisco@user-pc>
2018-02-20 20:43:16 -0600

    Agregando

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -9,6 +9,7 @@ package javaapplication8;
  *
  * @author diego
  */
+import java.util.Scanner;
 public class JavaApplication8 {
 
     /**
@@ -16,6 +17,43 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int [] arreglo = {4,5,6,8};
+              
+       
+       
+       int llave = 8;
+       int posicion =0;
+       
+        
+       for(int i = 1; i<arreglo.length;i++){
+           if(arreglo[i] == llave){
+               llave =  arreglo.length;
+               posicion = i;
+               
+               System.out.println("El valor de la llave =  " + posicion);
     }
-    
+       }
+       int numero;
+       int contador =0;
+       
+       Scanner ing = new Scanner(System.in);
+       System.out.print("Favor Ingrese Numero :");
+       numero = ing.nextInt();
+       
+       for (int i =1; i<(numero+1); i++){
+           if (numero %i==0){
+               contador ++;
+           }
+         
+        
+       }if(contador !=2){
+           System.out.println("False");}
+       else{
+           System.out.println("True");
+       
+       }
+       
+       
+       
+    }
 }
